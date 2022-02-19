25217a642b914de9535efbbdf059645ea1e476b6
Surajkunwar <kunwarsuraj19@gmail.com>
2022-02-19 14:21:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "/views")));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "index.html");
+});
+app.get("/download", (req, res) => {
+  var url = req.query.video_url;
+  console.log(url);
+  console.log(typeof url);
+  ytdl(url).pipe(fs.createWriteStream("./downloads/video.mp4"));
+});
+
+app.listen("3000", () => {
+  console.log("connected to port 3000");
+});
