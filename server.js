42bf2b55bb150f7768c4fea95a3251e4a360e82c
Surajkunwar <kunwarsuraj19@gmail.com>
2022-02-27 17:38:23 +0530

    finally

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, "/views")));
 
-app.get("https://ytdownloadapp.herokuapp.com//", (req, res) => {
+app.get("https://ytdownloadapp.herokuapp.com/", (req, res) => {
   res.sendFile(__dirname + "index.html");
 });
-app.get("https://ytdownloadapp.herokuapp.com//download", (req, res) => {
+app.get("https://ytdownloadapp.herokuapp.com/download", (req, res) => {
   const url = req.query.video_url;
   console.log(url);
   res.header("Content-Disposition", 'attachment; filename="video.mp4"');
